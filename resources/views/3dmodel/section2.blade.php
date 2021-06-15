eb6eab92a2af0d5f093322e3573a6447e340fe87
ZulZayn <zulwaqarzain96@gmail.com>
2021-06-15 17:14:38 +0800

    pulse

--- resources/views/3dmodel/section2.blade.php
+++ resources/views/3dmodel/section2.blade.php
@@ -1,4 +1,53 @@
-<img src="images/icons/pipeline2.png" class="p-1 pb-0 pt-2" style="width:100%;height:80%" alt="">
+@push('styles')
+<style>
+
+.blobs-container {
+  display: flex;
+}
+
+.blob {
+  background: black;
+  border-radius: 50%;
+  box-shadow: 0 0 0 0 rgba(0, 0, 0, 1);
+  margin: 8px;
+  height: 15px;
+  width: 15px;
+  transform: scale(1);
+  animation: pulse-black 2s infinite;
+}
+
+
+.blob.highlight {
+  background: #00a19c9e;
+  box-shadow: 0 0 0 0 #0179759e;
+  animation: pulse-highlight 2s infinite;
+}
+
+@keyframes pulse-highlight {
+  0% {
+    transform: scale(0.95);
+    box-shadow: 0 0 0 0 #00a19ca1;
+  }
+  
+  70% {
+    transform: scale(1);
+    box-shadow: 0 0 0 10px rgba(255, 82, 82, 0);
+  }
+  
+  100% {
+    transform: scale(0.95);
+    box-shadow: 0 0 0 0 rgba(255, 82, 82, 0);
+  }
+}
+
+</style>
+@endpush
+
+<img src="images/icons/pipeline2.png" class="p-1 pb-0 pt-2" style="width:100%;height:40px !important" alt="">
+
+<div class="blobs-container" style="position: absolute;top: 12.5%;left: 2.8%;">
+    <div class="blob highlight"></div>
+</div>
 
 <div class="row mb-0">
   <div class="col-4 ps-4">
